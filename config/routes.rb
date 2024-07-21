ad6b0faff56769539c2508be6388fc2c82bd896b
MohammedFalih <yunusfalih123@gmail.com>
2024-07-21 17:32:49 +0530

    fixed linting

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   resources :kittens
   root "kittens#index"
 
-
-
    get "up" => "rails/health#show", as: :rails_health_check
    get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
    get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
